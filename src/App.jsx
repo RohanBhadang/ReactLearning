2a81072908af29bcd9b565422ad141e5dac76acd
RohanBhadang <raorohan358@gmail.com>
2025-07-16 19:25:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,20 @@ function App() {
 
   return (
     <>
-      <div>
-       hello world</div>
+      <div class="w-full bg-black">
+        <h1>heading</h1>
+        <div class=" text-white text-2xl 
+font-weight: 700 px-8 py-8">
+           Google
+        </div>
+       <div>
+      {/* <img src="search.png" alt="Search" class="search-icon search-start" /> */}
+      <input type="text" class="search-box" placeholder="Search Google or type a URL" />
+      {/* <img src="mic.png" alt="Mic" class="search-icon mic-icon" />
+      <img src="lens.png" alt="Lens" class="search-icon lens-icon" /> */}
+    </div>
+       
+       </div>
     </>
   )
 }
